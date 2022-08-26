59a35531c054cd6a209e3ae8fecaeacf01bca5fc
Samuel Faulkner <samuel.faulkner@liberty-it.co.uk>
2022-08-26 17:27:00 +0100

    Starter tests for login and register forms

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +9,10 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -20,4 +25,29 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should require username', () => {
+    component.loginForm.setValue({
+      'username': "",
+      "password": "testPassword123"
+    });
+    expect(component.loginForm.valid).toEqual(false);
+  });
+
+  it('should require password', () => {
+    component.loginForm.setValue({
+      'username': "TestUsername123",
+      "password": ""
+    });
+    expect(component.loginForm.valid).toEqual(false);
+  });
+
+  it('should require both username and password', () => {
+    component.loginForm.setValue({
+      'username': "TestUsername123",
+      "password": "testpassword123"
+    });
+    expect(component.loginForm.valid).toEqual(true);
+  });
+
 });
