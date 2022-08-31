9de01af2d5752dc0903f75800015d7efb38ede32
Samuel Faulkner <samuel.faulkner@liberty-it.co.uk>
2022-08-31 09:47:14 +0100

    Extra register form tests

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Form, FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 
 function checkMatchedPasswordsValidation(fg: FormGroup): ValidationErrors | null {
