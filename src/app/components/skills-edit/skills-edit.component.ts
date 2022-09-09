d111b7b19f4922d519fcf1f549087d364ed221c4
Samuel Faulkner <samuel.faulkner@liberty-it.co.uk>
2022-09-09 17:14:29 +0100

    Add skills

--- src/app/components/skills-edit/skills-edit.component.ts
+++ src/app/components/skills-edit/skills-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../services/user.service";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-skills-edit',
@@ -10,16 +10,32 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class SkillsEditComponent implements OnInit {
 
   profile: any;
-  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
+  skills: any;
+  skillForm: FormGroup;
+  hasSuccess: boolean = false;
+  successMessage: string = "";
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) {
+    this.skillForm = formBuilder.group({
+      skill: ['', Validators.required],
+      description: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
     this.userService.getUserAccount().subscribe((res) => {
-      this.profile = res;
+      this.skills = res.userProfile.skills;
     });
   }
 
   onSkillAdd() {
+    let skill = <string>this.skillForm.get('skill')?.value;
+    let description =  <string>this.skillForm.get('description')?.value;
 
+    this.userService.addSkillToUser(skill, description).subscribe((res) => {
+      this.hasSuccess = true;
+      this.successMessage = "Skill added successfully";
+    });
   }
 
   onSkillDelete() {
